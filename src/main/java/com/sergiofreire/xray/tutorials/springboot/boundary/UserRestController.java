75fbd3baeba33d7257f345a347cc043d52c5d724
Sergio Freire <sergio.freire@gmail.com>
2025-05-06 11:57:35 +0100

    ST-2 dummy bug

--- src/main/java/com/sergiofreire/xray/tutorials/springboot/boundary/UserRestController.java
+++ src/main/java/com/sergiofreire/xray/tutorials/springboot/boundary/UserRestController.java
@@ -40,7 +40,7 @@ public class UserRestController {
     @GetMapping(path="/users" )
     public List<User> getAllUsers() {
         // return userService.getAllUsers();
-        return null; // bug on purpose
+        return null; // bug on purpose...
     }
 
     @DeleteMapping("/users/{id}")
