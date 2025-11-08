32dada9237c10e1be911cb970307d6eee298faea
Sergio Freire <sergio.freire@gmail.com>
2025-11-08 16:48:49 +0000

    k6 - 95% requests below 1s

--- src/test/k6/basic_performance_test.js
+++ src/test/k6/basic_performance_test.js
@@ -9,7 +9,7 @@ export const options = {
   ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<500'], // 95% of requests should be below 500ms
+    http_req_duration: ['p(95)<1000'], // 95% of requests should be below 1s
   },
 };
 
